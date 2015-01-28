2238bae84718da10841a1510e6a4ea2d10f812f6
Giovanni Mondo <giovanni.mondo@yahoo.it>
2015-01-28 21:44:15 +0100

    First minimal working release, easy but memory wasting

--- anstrainer.c
+++ anstrainer.c
@@ -13,36 +13,105 @@
 #include <time.h>
 #include <string.h>
 #include "anstrainer.h"
-#define MAX_ANS 16
+#define MAX_ANS 5
 
 int irand(int max);
-char* make_query();
+void make_query(char *ans);
 
 int main() {
+	char right_ans[MAX_ANS];
+	char user_ans[MAX_ANS]; 
+	int score = 0;
+
+	/* initialize random generator */
 	srand(clock());
 
 /* debug logs
+	printf("Number of contexts: %d\n", sizeof(contexts)/sizeof(contexts[0]));
 	printf("%s\n", contexts[0].query_answers[0].query);
 	printf("%d\n", contexts[0].size);
 	printf("%s\n", contexts[1].query_answers[1].query);
-	printf("%d\n", sizeof(contexts)/sizeof(contexts[0]));
 	printf("%d\n", irand(3));
 	make_query();
 */
-	char *res=make_query();
-	char ans[MAX_ANS];
-	scanf("%s", ans);
-	if ( 0 == strcmp(res, ans) ) {
-		printf("That's correct!\n");	
-	} else {
-		printf("That's wrong, the right answer is %s", res);
-	}
-}
+	printf("Welcome to Answer Trainer!\n");
+	printf("Answer with the each digit containing a right responses or - if none is present.\n\n");
 
-char* make_query() {
-	printf("This is the question");
-	return "123";
-}
+	do {
+		make_query(right_ans);
+		scanf("%s", user_ans);
+		if ( 0 == strcmp(right_ans, user_ans) ) {
+			score++;
+			printf("That's correct!\n");	
+		} else {
+			score--;
+			printf("That's wrong, the right answer is %s\n", right_ans);
+		}
+		printf("Your score is %d, continue? [Y/n]", score);
+		scanf("%s", user_ans);
+	} while ( 0 != strcmp("n", user_ans));
+} /* main */
+
+int tot_ctxts = sizeof(contexts)/sizeof(contexts[0]);
+void make_query(char *ans) {
+	int ctx_idx = irand(tot_ctxts);
+	int right_idx = irand(contexts[ctx_idx].size);
+	int icount, jcount;
+	int rnd_idx;
+	int res_idx;
+	int res_len = 0;
+	char responses[MAX_ANS][MAX_RESPONSE_LEN];
+	char res[MAX_ANS];
+
+	printf("%s\n", contexts[ctx_idx].query_answers[right_idx].query);
+	for (icount = 0; icount < MAX_ANS; icount++) {
+		res[icount] = 0;
+		rnd_idx = irand(contexts[ctx_idx].size);
+		res_idx = irand(contexts[ctx_idx].query_answers[rnd_idx].size);
+		sprintf(responses[icount], "%s", contexts[ctx_idx].query_answers[rnd_idx].answers[res_idx]);
+/*
+*/
+printf("%d) %s\n", icount+1, responses[icount]);
+		if (rnd_idx == right_idx) {
+			res[icount] = 1;
+		} else {
+			/* check if another question has the same response */
+			for (jcount = 0; jcount < contexts[ctx_idx].query_answers[right_idx].size; jcount++) {
+				if (0 == strcmp(contexts[ctx_idx].query_answers[right_idx].answers[jcount], contexts[ctx_idx].query_answers[rnd_idx].answers[res_idx])) {
+					res[icount] = 1;
+					break;
+				}
+			}		
+		}
+	}
+	/* remove duplicated responses */
+	for (icount = 0; icount < MAX_ANS - 1; icount++) {
+		for (jcount = icount + 1; jcount < MAX_ANS; jcount++) {
+			if (0 == strcmp(responses[icount], responses[jcount])) {
+/*
+*/
+printf("removing %d\n", jcount);
+				responses[jcount][0]=0;
+				res[jcount] = 0;
+			}
+		}
+	}
+	/* print responses and prepare answer */
+	icount = 1;
+	for (jcount = 0; jcount < MAX_ANS; jcount++) {
+		if (responses[jcount][0]) {
+			if (res[jcount]) {
+				ans[res_len++]='0' + icount;
+			}
+			printf("%d) %s\n", icount++, responses[jcount]);
+		}
+	}
+	if (0 == res_len) {
+		ans[res_len++] = '-';
+	}
+	ans[res_len] = '\0';
+	return;
+} /* make_query */
 
 int irand(int max) {
 	return rand() % max;
--- anstrainer.h
+++ anstrainer.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #define MAX_RESPONSE_LEN 80
 /* Try to have context of same size to save program size */
-#define MAX_CONTEXT_LEN 3 	
+#define MAX_CONTEXT_LEN 4 	
 
 typedef struct query_answers {
 	char 	*query;
+	int	size;
 	char 	*answers[MAX_RESPONSE_LEN];
 } t_qa;
 
@@ -17,26 +18,24 @@ typedef struct context {
 t_ctxt contexts[] = {
 	{
 		/* Number of questions in the context */
-		2, 
+		4,
 		{
-			/* Question and answers. All answers in a context must be different */ 
-			{"uno", {"un", "one"}},
-			{"due", {"deux", "two"}}
+			/* Question and answers */
+			/* First example: questions with single response */
+			{"What's the capital of France?", 1, {"Paris"}},
+			{"What's the capital of Italy?", 1, {"Rome"}},
+			{"What's the capital of Denmark?", 1, {"Copenaghen"}},
+			{"What's the capital of Spain?", 1, {"Madrid"}}
 		}
 	},
 	{
 		3,
 		{
-			{"un", {"uno", "one"}},
-			{"deux", {"due", "two"}},
-			{"trois", {"tre", "three"}}
+			/* Second example: questions with multiple responses */
+			/* Note that same response cannot be written differently, i.e. Germany differs from germany */
+			{"In which states flows Danube?", 3, {"Austria", "Romania", "Germany"}},
+			{"In which states flows Rhine?", 3, {"Switzerland", "France", "Germany"}},
+			{"Which states border Germany?", 4, {"France", "Poland", "Switzerland", "Austria"}} 
 		}
-	},
-	{
-		2,
-		{
-			{"one", {"un", "uno"}},
-			{"two", {"deux", "due"}}
-		}	
 	}
 };
